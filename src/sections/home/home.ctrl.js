0818edc09f1a35d4e293a64ff04f00f1b7945bae
Nirjal Khadka <nirjal@gmail.com>
2016-05-04 16:28:21 +0200

    remove services and factories from outside folders, added new service

--- src/sections/home/home.ctrl.js
+++ src/sections/home/home.ctrl.js
@@ -1,36 +1,13 @@
-angular.module('app.core').controller('HomeController',function(){
+angular.module('app.core').controller('HomeController',function(MyService){
     var vm = this;   
     vm.firstName = "Nirjal";
     vm.lastName = "Khadka";    
     vm.title = 'Nirjals angular app';
     vm.searchInput = '';
     console.log('nu är vi i home controller');
+    
+    vm.data = MyService.getData();
+    console.log(vm.data );
   
-    vm.shows = [
-        {
-            title: 'Game of Thrones',
-            year: 2011,
-            favorite: true
-        },
-        {
-            title: 'Walking Dead',
-            year: 2010,
-            favorite: false
-        },
-        {
-            title: 'Firefly',
-            year: 2002,
-            favorite: true
-        },
-        {
-            title: 'Banshee',
-            year: 2013,
-            favorite: true
-        },
-        {
-            title: 'Greys Anatomy',
-            year: 2005,
-            favorite: false
-        }
-    ];
+    
 });
---
+++ src/services/user.srv.js
@@ -0,0 +1,37 @@
+angular.module('app.services').factory('MyService', userService );
+
+
+function userService() {
+   var fac = {};
+   var data = [
+            {
+                title: 'Game of Thrones',
+                year: 2011,
+                favorite: true
+            },
+            {
+                title: 'Walking Dead',
+                year: 2010,
+                favorite: false
+            },
+            {
+                title: 'Firefly',
+                year: 2002,
+                favorite: true
+            },
+            {
+                title: 'Banshee',
+                year: 2013,
+                favorite: true
+            },
+            {
+                title: 'Greys Anatomy',
+                year: 2005,
+                favorite: false
+            }
+     ];
+     fac.getData = function () {
+         return data;
+     }
+     return fac; 
+}
