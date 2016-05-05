e51438d3ea917244d910e41d7ad5b3bf31f52c96
Nirjal Khadka <nirjal@gmail.com>
2016-05-05 23:14:58 +0200

    made changes to the view from the api

--- src/services/user.srv.js
+++ src/services/user.srv.js
@@ -2,35 +2,8 @@ angular.module('app.services').factory('MyService', userService);
 
 
 function userService($http, randomUserUrl) {
-    var fac = {};
-    var data = [
-            {
-                title: 'Game of Thrones',
-                year: 2011,
-                favorite: true
-            },
-            {
-                title: 'Walking Dead',
-                year: 2010,
-                favorite: false
-            },
-            {
-                title: 'Firefly',
-                year: 2002,
-                favorite: true
-            },
-            {
-                title: 'Banshee',
-                year: 2013,
-                favorite: true
-            },
-            {
-                title: 'Greys Anatomy',
-                year: 2005,
-                favorite: false
-            }
-     ];
-    fac.getShows = function () {
+    var fac = {};  
+    fac.getUser = function () {
          return $http({
             'url': randomUserUrl,
             'method':'get',
@@ -39,8 +12,7 @@ function userService($http, randomUserUrl) {
                 },             
             'cache':true
         }).then(function(response){
-           console.log(response);
-            return response;
+            return response.data.results;
         }); 
     }
     return fac;
