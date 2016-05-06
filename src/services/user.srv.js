71c5b2dfbc1b4f17c6baa1974c8885840849b389
Nirjal Khadka <nirjal@gmail.com>
2016-05-06 10:00:50 +0200

    no message

--- src/services/user.srv.js
+++ src/services/user.srv.js
@@ -2,36 +2,18 @@ angular.module('app.services').factory('MyService', userService );
 
 
 function userService() {
-   var fac = {};
-   var data = [
-            {
-                title: 'Game of Thrones',
-                year: 2011,
-                favorite: true
-            },
-            {
-                title: 'Walking Dead',
-                year: 2010,
-                favorite: false
-            },
-            {
-                title: 'Firefly',
-                year: 2002,
-                favorite: true
-            },
-            {
-                title: 'Banshee',
-                year: 2013,
-                favorite: true
-            },
-            {
-                title: 'Greys Anatomy',
-                year: 2005,
-                favorite: false
-            }
-     ];
-     fac.getShows = function () {
-         return data;
-     }
-     return fac; 
+ var fac = {};  
+    fac.getUser = function () {
+         return $http({
+            'url': randomUserUrl,
+            'method':'get',
+            'headers':{
+                'Content-type':'application/json'
+                },             
+            'cache':true
+        }).then(function(response){
+            return response.data.results;
+        }); 
+    }
+    return fac;
 }
